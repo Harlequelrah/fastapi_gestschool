0bdba44eb9dee599517d8e26c0ccc4a19a788df5
Harlequelrah <degbovimax19@gmail.com>
2024-12-31 02:27:48 +0000

    creation de schemas pour Profile

--- fastapi_gestschool/profile/schema.py
+++ fastapi_gestschool/profile/schema.py
@@ -1,7 +1,28 @@
-# from pydantic import BaseModel, Field
-# from typing import List, Optional
-# from datetime import datetime
-# from decimal import Decimal
-# from fastapi import Form
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from datetime import datetime
+from decimal import Decimal
+from fastapi import Form
 
+from .enum import GenderEnum
+class ProfileBase(BaseModel):
+    age:int=Field(example=20)
+    gender:GenderEnum=Field(example=GenderEnum.Female, default=GenderEnum.NOT_SPECIFIED)
+    birth_date: datetime = Field(example=datetime.datetime(2000, 1, 1, 10, 30))
 
+class ProfileCreate(ProfileBase):
+    teacher_id:int=Field(example=1)
+class ProfilUpdate(BaseModel):
+    teacher_id: Optional[int] = Field(example=1)
+    gender:Optional[GenderEnum] = Field(example=GenderEnum.Male)
+    birth_date: Optional[datetime] = Field(example=datetime.datetime(2000, 1, 1, 10, 30))
+    age:Optional[int]=Field(example=30)
+
+class ProfilePydanticModel(BaseModel):
+    id:int
+    age:int
+    gender:GenderEnum
+    birth_date:datetime
+    teacher_id:int
+    class setting :
+        from_orm=True
